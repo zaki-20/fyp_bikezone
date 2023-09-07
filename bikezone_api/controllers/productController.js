12f60066099b877701c5a4cec0372a8ff4c9ab14
Zakria <zakigame199@gmail.com>
2023-09-07 18:54:12 +0500

    product Api's - getAll, getSingle, update, create, delete

--- bikezone_api/controllers/productController.js
+++ bikezone_api/controllers/productController.js
@@ -1,5 +1,72 @@
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const Product = require("../models/productModel");
+
+
+//create product --admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+    // req.body.user = req.user.id
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product,
+    });
+});
 
 // Get All Product
-exports.getAllProducts = (req, res) => {
-    res.json("response success")
-}
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        sucess: true,
+        products
+    })
+
+})
+
+
+//get product details
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+
+
+    const product = await Product.findById(req.params.id);
+  
+    // if (!product) {
+    //   return next(new ErrorHandler("product not found", 404));
+    // }
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  });
+
+//update products --Admin
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    // if (!product) {
+    //   return next(new ErrorHandler("product not found", 404));
+    // }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  });
+
+
+  //delete product
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    // if (!product) {
+    //   return next(new ErrorHandler("product not found", 404));
+    // }
+    await product.deleteOne();
+    res.status(200).json({
+      success: true,
+      message: "product has been deleted ",
+    });
+  });
+  
--- bikezone_api/routes/productRoute.js
+++ bikezone_api/routes/productRoute.js
@@ -1,9 +1,21 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
+//create new product
+router.route("/admin/product/new").post( createProduct);
+
+//get single product details
+router.route("/product/:id").get(getProductDetails);
+
+//get all products
 router.route("/products").get(getAllProducts);
 
+//update products
+router
+  .route("/admin/product/:id")
+  .put(updateProduct)
+  .delete(deleteProduct)
 
 module.exports = router
