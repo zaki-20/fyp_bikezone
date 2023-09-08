23e828622c7e3cef4496eed346d1cc259b744810
Zakria <zakigame199@gmail.com>
2023-09-08 21:23:43 +0500

    user login Api

--- bikezone_api/controllers/userController.js
+++ bikezone_api/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require("../models/userModel");
 // const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
-//create user 
+//create user  with jwt
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     const { name, email, password } = req.body
     const user = await User.create({
@@ -21,5 +21,33 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         
     })
 
+})
+
+//login user 
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    //check if user gives emailand password both
+    if (!email || !password) {
+        return next(new ErrorHandler("please enter username and password", 400))
+    }
+
+    const user = await User.findOne({ email }).select("+password")
+    if (!user) {
+        return next(new ErrorHandler("invalid email or password", 401))
+    }
+
+    const isPasswordMatch = await user.comparePassword(password)
+
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler("invalid email or password", 401))
+    }
+
+    const token = user.getJWTToken()
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+    
 })
 
--- bikezone_api/models/userModel.js
+++ bikezone_api/models/userModel.js
@@ -61,4 +61,9 @@ userSchema.methods.getJWTToken = function () {
     })
 }
 
+//compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
 module.exports = mongoose.model("User", userSchema);
