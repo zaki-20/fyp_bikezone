d961eb90e46dce4a73043300d1a2715e1f9ef8db
Zakria <zakigame199@gmail.com>
2023-09-10 00:39:24 +0500

    get all orders, delete order, update order status -Api's

--- bikezone_api/controllers/orderController.js
+++ bikezone_api/controllers/orderController.js
@@ -56,3 +56,79 @@ exports.myOrder = catchAsyncErrors(async (req, res, next) => {
         orders,
     });
 });
+
+// get all Orders -- Admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+
+    orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+    });
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders,
+    });
+});
+
+// update Order status  -- Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+
+    if (order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("you have already delieverd this order", 400))
+    }
+
+    order.orderItems.forEach(async (o) => {
+        await updateStock(o.product, o.quantity, next)
+    })
+
+    order.orderStatus = req.body.status;
+
+    if (req.body.status === "delivered") {
+        order.deliveredAt = Date.now()
+    }
+
+    await order.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+    });
+
+});
+
+//Fucntion for update stock===========
+async function updateStock(id, quantity, next) {
+    
+    const product = await Product.findById(id);
+
+    // if (quantity > product.Stock) {    
+    //   return next(new ErrorHandler("Insufficient stock", 400));
+    // }
+    product.Stock -= quantity;
+
+    await product.save({ validateBeforeSave: false });
+} 
+
+// delete Order -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+
+    await order.deleteOne();
+
+    res.status(200).json({
+        success: true,
+    });
+});
--- bikezone_api/routes/orderRoute.js
+++ bikezone_api/routes/orderRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-    newOrder, getSingleOrder, myOrder
+    newOrder, getSingleOrder, myOrder, getAllOrders, updateOrder, deleteOrder
 } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 const router = express.Router();
@@ -11,6 +11,16 @@ const router = express.Router();
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrder);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
 
 
 module.exports = router;
