743b5726a3e7ea6b5cbf592d46cf5fbe732c11a8
Zakria <zakigame199@gmail.com>
2023-09-09 08:40:54 +0500

    update profile Api

--- bikezone_api/routes/userRoute.js
+++ bikezone_api/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser, logout, forgotPassword,
-    resetPassword, getUserDetails, updatePassword
+    resetPassword, getUserDetails, updatePassword, updateProfile
 } = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middlewares/auth");
 
@@ -13,7 +13,7 @@ router.route("/password/forgot").post(forgotPassword)
 router.route("/password/reset/:token").put(resetPassword)
 router.route("/me").get(isAuthenticatedUser, getUserDetails)
 router.route("/password/update").put(isAuthenticatedUser, updatePassword)
-
+router.route("/me/update").put(isAuthenticatedUser, updateProfile)
 router.route("/logout").post(logout)
 
 
