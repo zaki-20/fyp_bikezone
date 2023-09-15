eb47c7acd0d9601db7bca383d8416b5661b26d14
Zakria <zakigame199@gmail.com>
2023-09-15 10:03:19 +0500

    getWorkshopDetails, getAllWorkshops -Api's

--- bikezone_api/controllers/workshopController.js
+++ bikezone_api/controllers/workshopController.js
@@ -26,36 +26,73 @@ exports.createWorkshop = catchAsyncErrors(async (req, res, next) => {
 
 });
 
-exports.updateWorkshop = catchAsyncErrors( async (req, res, next) => {
- 
+exports.updateWorkshop = catchAsyncErrors(async (req, res, next) => {
+
     let workshop = await Workshop.findById(req.params.id);
 
     if (!workshop) {
-      return next(new ErrorHandler("Workshop not found", 404));
+        return next(new ErrorHandler("Workshop not found", 404));
     }
 
     workshop = await Workshop.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
         useFindAndModify: false,
-      });
+    });
 
     await workshop.save();
 
     res.status(200).json({
-      statusCode: 200,
-      success: true,
-      message: "Workshop updated successfully",
-      payload: { workshop },
+        statusCode: 200,
+        success: true,
+        message: "Workshop updated successfully",
+        payload: { workshop },
     });
 
 });
 
-exports.deleteWorkshop = catchAsyncErrors( async (req, res, next) => {
-  
+exports.deleteWorkshop = catchAsyncErrors(async (req, res, next) => {
+
     const { id } = req.params;
 
     const workshop = await Workshop.findByIdAndRemove(id);
+
+    if (!workshop) {
+        return next(new ErrorHandler("Workshop not found", 404));
+    }
+
+    res.status(200).json({
+        statusCode: 200,
+        success: true,
+        message: "Workshop deleted successfully",
+    });
+
+});
+
+exports.getAllWorkshops = catchAsyncErrors(async (req, res, next) => {
+
+    const workshopCount = await Workshop.countDocuments();
+    const workshops = await Workshop.find();
+
+
+    if (!workshops) {
+        return next(new ErrorHandler("Workshop not found", 404));
+    }
+
+    res.status(200).json({
+        statusCode: 200,
+        success: true,
+        message: "All workshops retrieved successfully",
+        payload: { workshops, workshopCount },
+    });
+
+});
+
+exports.getWorkshopDetails = async (req, res, next) => {
+  
+    const { id } = req.params;
+
+    const workshop = await Workshop.findById(id);
 
     if (!workshop) {
       return next(new ErrorHandler("Workshop not found", 404));
@@ -64,43 +101,8 @@ exports.deleteWorkshop = catchAsyncErrors( async (req, res, next) => {
     res.status(200).json({
       statusCode: 200,
       success: true,
-      message: "Workshop deleted successfully",
+      message: "Workshop retrieved successfully",
+      payload: { workshop },
     });
  
-});
-
-// export const getAllWorkshops = async (req, res, next) => {
-//   try {
-//     const workshops = await Workshop.find();
-
-//     res.status(200).json({
-//       statusCode: 200,
-//       success: true,
-//       message: "All workshops retrieved successfully",
-//       payload: { workshops },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// export const getWorkshopById = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-
-//     const workshop = await Workshop.findById(id);
-
-//     if (!workshop) {
-//       return next(new ErrorHandler("Workshop not found", 404));
-//     }
-
-//     res.status(200).json({
-//       statusCode: 200,
-//       success: true,
-//       message: "Workshop retrieved successfully",
-//       payload: { workshop },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+};
--- bikezone_api/routes/workshopRoute.js
+++ bikezone_api/routes/workshopRoute.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const { createWorkshop, updateWorkshop, deleteWorkshop } = require("../controllers/workshopController")
+const { createWorkshop, updateWorkshop, deleteWorkshop, getAllWorkshops, getWorkshopDetails } = require("../controllers/workshopController")
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 const router = express.Router()
 
 
-router.route("/admin/workshop/new").post(createWorkshop)
-router.route("/admin/workshop/:id")
-    .put(updateWorkshop)
-    .delete(deleteWorkshop)
+
+router.route("/workshops").get(isAuthenticatedUser, getAllWorkshops)
+
+router.route("/workshop/new").post(isAuthenticatedUser, authorizeRoles('workshopOwner'), createWorkshop)
+router.route("/workshop/:id")
+    .get(getWorkshopDetails)
+    .put(isAuthenticatedUser, authorizeRoles('workshopOwner'),updateWorkshop)
+    .delete(isAuthenticatedUser, authorizeRoles('workshopOwner'),deleteWorkshop)
 
 
 module.exports = router
